5530ca0911d16a301e767eaf811c9e6d33c6b531
Raul Lama <tenta@Rauls-MacBook-Air.local>
2025-11-24 14:12:24 +0545

    Rename folder from Task 5 to Task5

--- Task 5/obj/Debug/net9.0/Task 5.AssemblyInfo.cs
+++ Task 5/obj/Debug/net9.0/Task 5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task 5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c072b98a515eed690c65703350d71c0529e1cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task 5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task 5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
